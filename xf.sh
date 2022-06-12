5729033de5f1b972298f53a3dfc454fab0e69693
beyqcnh <qcnh01@163.com>
2022-06-12 15:14:12 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "29115fe9-3091-4f35-a54c-2374709d20a8",
                         "alterId": 0
                     }
                 ],
